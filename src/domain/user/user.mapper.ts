032c1478bed3312c1b45e870421695b0e547ecc6
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-06 20:22:10 -0300

    feat: add domainToTypeOrm method in UserMapper for user entity transformation

--- src/domain/user/user.mapper.ts
+++ src/domain/user/user.mapper.ts
@@ -3,6 +3,16 @@ import { User } from './user.domain';
 import { UserFactory } from './user.factory';
 
 export class UserMapper {
+  public static domainToTypeOrm(user: User) {
+    return {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+      password: user.password,
+      createdAt: user.createdAt,
+    };
+  }
+
   public static typeOrmToDomain(user: UserModel) {
     return UserFactory.create(user);
   }
