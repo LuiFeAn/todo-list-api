2236e4208aa8b13cecd40272ce110d5a89911eef
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-06 18:04:07 -0300

    test: enhance UserEntity unit tests with additional validation cases

--- src/domain/user/user.unit.spec.ts
+++ src/domain/user/user.unit.spec.ts
@@ -2,11 +2,11 @@ import { randomUUID } from 'crypto';
 import { User } from './user.domain';
 import { UserMapper } from './user.mapper';
 
-describe('UserEntity unit tests', () => {
-  it('Should create a user', () => {
+describe('UserEntity Unit Tests', () => {
+  it('should create a user successfully', () => {
     const input = {
       id: randomUUID(),
-      email: 'teste@email.com',
+      email: 'test@example.com',
       username: 'Luis Fernando',
       password: '12345',
       createdAt: new Date().toISOString(),
@@ -18,10 +18,98 @@ describe('UserEntity unit tests', () => {
     expect(user).toBeTruthy();
   });
 
-  it('Should throw an error if the email is invalid', () => {
+  it('should throw an error if the UUID is empty', () => {
+    const input = {
+      id: '',
+      email: 'test@example.com',
+      username: 'Luis Fernando',
+      password: '12345',
+      createdAt: new Date().toISOString(),
+    };
+
+    expect(() => new User(input)).toThrow('_id should not be empty');
+  });
+
+  it('should throw an error if the UUID is invalid', () => {
+    const input = {
+      id: '12345',
+      email: 'test@example.com',
+      username: 'Luis Fernando',
+      password: '12345',
+      createdAt: new Date().toISOString(),
+    };
+
+    expect(() => new User(input)).toThrow('_id must be a UUID');
+  });
+
+  it('should throw an error if the username is empty', () => {
     const input = {
       id: randomUUID(),
-      email: 'testeemail.com',
+      email: 'test@example.com',
+      username: '',
+      password: '12345',
+      createdAt: new Date().toISOString(),
+    };
+
+    expect(() => new User(input)).toThrow('_username should not be empty');
+  });
+
+  it('should throw an error if the username exceeds 250 characters', () => {
+    const input = {
+      id: randomUUID(),
+      email: 'test@example.com',
+      username: 'Luis'.repeat(250),
+      password: '12345',
+      createdAt: new Date().toISOString(),
+    };
+
+    expect(() => new User(input)).toThrow(
+      '_username must be shorter than or equal to 250 characters',
+    );
+  });
+
+  it('should throw an error if the password is empty', () => {
+    const input = {
+      id: randomUUID(),
+      email: 'test@example.com',
+      username: 'Luis Fernando',
+      password: '',
+      createdAt: new Date().toISOString(),
+    };
+
+    expect(() => new User(input)).toThrow('_password should not be empty');
+  });
+
+  it('should throw an error if the email is invalid', () => {
+    const input = {
+      id: randomUUID(),
+      email: 'testemail.com',
+      username: 'Luis Fernando',
+      password: '12345',
+      createdAt: new Date().toISOString(),
+    };
+
+    expect(() => new User(input)).toThrow('_email must be an email');
+  });
+
+  it('should throw an error if the email is empty', () => {
+    const input = {
+      id: randomUUID(),
+      email: '',
+      username: 'Luis Fernando',
+      password: '12345',
+      createdAt: new Date().toISOString(),
+    };
+
+    expect(() => new User(input)).toThrow(
+      '_email should not be empty,_email must be an email',
+    );
+  });
+
+  it('should throw an error if the email format is invalid', () => {
+    const input = {
+      id: randomUUID(),
+      email: 'testemail.com',
       username: 'Luis Fernando',
       password: '12345',
       createdAt: new Date().toISOString(),
