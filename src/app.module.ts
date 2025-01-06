aabf98e9b0d7c5a34d564bdaa57135faa65da6e8
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-06 19:29:23 -0300

    feat: configure TypeORM and JWT modules with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,33 @@
+import { UserModel } from '@infra/user/user.model';
+import { UserModule } from '@infra/user/user.module';
 import { Module } from '@nestjs/common';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        database: configService.get<string>('DB_NAME'),
+        host: configService.get<string>('DB_HOST'),
+        username: configService.get<string>('DB_USER'),
+        port: configService.get<number>('DB_PORT'),
+        password: configService.get<string>('DB_PASS'),
+        entities: [UserModel],
+      }),
+    }),
+    JwtModule.register({
+      global: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [UserModule],
 })
 export class AppModule {}
