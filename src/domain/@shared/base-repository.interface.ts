aeb0f6bed3caa078d4517346f1f0f74720ac49a4
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-06 18:45:39 -0300

    feat: add IBaseRepository interface for defining repository structure and operations

---
+++ src/domain/@shared/base-repository.interface.ts
@@ -0,0 +1,15 @@
+import {
+  IRepositoryMetaDataInput,
+  IRepositoryMetaDataOutput,
+} from './repository-meta-data.interface';
+
+export interface IBaseRepository<E> {
+  create(entity: E): Promise<void>;
+  findById(id: string): Promise<E>;
+  findAll(
+    data: Partial<E>,
+    pagination: IRepositoryMetaDataInput,
+  ): Promise<IRepositoryMetaDataOutput<E>>;
+  update(id: string, data: Partial<E>): Promise<void>;
+  delete(id: string): Promise<void>;
+}
