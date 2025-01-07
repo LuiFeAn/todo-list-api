709f60352f88a974d97d0f74c1ea75a9e22cfc60
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-07 11:17:32 -0300

    feat: add CreateTodoInputDto for creating todos with validation

---
+++ src/use-cases/todo/create-todo.dto.ts
@@ -0,0 +1,35 @@
+import { PriorityEnum } from '@domain/todo/priority.enum';
+import { entityValidator } from '@utils/entityValdiator';
+import { IsEnum, IsNotEmpty, IsOptional, MaxLength } from 'class-validator';
+
+export class CreateTodoInputDto {
+  @IsNotEmpty()
+  @MaxLength(100)
+  title: string;
+
+  @IsNotEmpty()
+  @MaxLength(800)
+  description: string;
+
+  @IsOptional()
+  @IsEnum(PriorityEnum)
+  priority: PriorityEnum;
+
+  constructor({
+    title,
+    description,
+    priority,
+  }: {
+    title: string;
+    description: string;
+    priority: PriorityEnum;
+  }) {
+    if (title || description || priority) {
+      this.validate();
+    }
+  }
+
+  private validate() {
+    entityValidator(this);
+  }
+}
