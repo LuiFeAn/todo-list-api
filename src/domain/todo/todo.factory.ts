b5789dd906943446b50bbab58269f37af8b1c526
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-07 11:21:45 -0300

    feat: update TodoListFactory to include optional priority with default value

--- src/domain/todo/todo.factory.ts
+++ src/domain/todo/todo.factory.ts
@@ -1,18 +1,21 @@
 import { randomUUID } from 'crypto';
 import { TodoList } from './todo.domain';
 import { ICreateTodoFactoryInput } from './todo.factory.interface';
+import { PriorityEnum } from './priority.enum';
 
 export class TodoListFactory {
   public static create({
     id,
     title,
     description,
+    priority,
     createdAt,
   }: ICreateTodoFactoryInput) {
     return new TodoList({
       id: id ?? randomUUID(),
       title,
       description,
+      priority: priority ?? PriorityEnum.Low,
       createdAt: createdAt ?? new Date().toISOString(),
     });
   }
