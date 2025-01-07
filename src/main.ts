6964d05067b2e442626b1156ded2642c78401aa0
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-07 15:23:25 -0300

    feat: enable CORS for the application

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { envs } from '@infra/@shared/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.enableVersioning({
     type: VersioningType.URI,
   });
